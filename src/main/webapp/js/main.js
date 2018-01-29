f7dc7b6753bb9ead1ed4ea43a7642d9349bfedf3
matthias avinee <matthias.avinee@hei.yncrea.fr>
2018-01-29 14:13:41 +0000

    a

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -14,7 +14,6 @@ function openCity(evt, cityName) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
