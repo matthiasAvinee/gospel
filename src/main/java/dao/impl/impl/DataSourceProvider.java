15c2bf66969159c1037c41403e266475c2eb7f9f
qlejour <quentin.lejour@hei.yncrea.fr>
2018-03-27 21:29:29 +0200

    update de la bdd

--- src/main/java/dao/impl/impl/DataSourceProvider.java
+++ src/main/java/dao/impl/impl/DataSourceProvider.java
@@ -5,7 +5,8 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import javax.sql.DataSource;
 
 public class DataSourceProvider {
-    private static MysqlDataSource dataSource;
+
+    /*private static MysqlDataSource dataSource;
 
     public static DataSource getDataSource() {
         if (dataSource == null) {
@@ -17,7 +18,19 @@ public class DataSourceProvider {
             dataSource.setPassword("root");
         }
         return dataSource;
+    }*/
+
+    private static MysqlDataSource dataSource;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new MysqlDataSource();
+            dataSource.setServerName("eu-cdbr-west-02.cleardb.net");
+            dataSource.setPort(3306);
+            dataSource.setDatabaseName("heroku_9fc24712e36bb44");
+            dataSource.setUser("b34c4abb5e8eac");
+            dataSource.setPassword("01cd2437");
+        }
+        return dataSource;
     }
-
-
 }
