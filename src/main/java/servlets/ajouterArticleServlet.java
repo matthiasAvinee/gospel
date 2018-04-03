75a188385dada29a634f9e5f25004f2d8e107db7
matthias Avinee <matthias.avinee@hei.yncrea.fr>
2018-04-03 16:05:41 +0200

    a

--- src/main/java/servlets/ajouterArticleServlet.java
+++ src/main/java/servlets/ajouterArticleServlet.java
@@ -16,9 +16,7 @@ import java.io.IOException;
 public class ajouterArticleServlet extends AbstractGenericServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       // TemplateEngine templateEngine = this.createTemplateEngine(req);
-       // resp.setContentType("text/html;charset=UTF-8");
-       // req.setCharacterEncoding("UTF-8");
+
         WebContext context = new WebContext(req, resp, getServletContext());
 
         context.setVariable("error", req.getSession().getAttribute("errorMessage"));
--- src/main/java/servlets/supprimerEvenementServlet.java
+++ src/main/java/servlets/supprimerEvenementServlet.java
@@ -15,8 +15,7 @@ public class supprimerEvenementServlet extends AbstractGenericServlet{
 
 
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            resp.setContentType("text/html;charset=UTF-8");
-            req.setCharacterEncoding("UTF-8");
+
 
             int id = Integer.parseInt(req.getParameter("id"));
             EvenementLibrary.getInstance().supprimerevenement(id);
